a17046a7849a81a3e45eb7211a189d011891ac28
yoelferreyra <yoelferreyra24@gmail.com>
2024-04-11 23:50:06 -0300

    Adding routes get put delete post

--- app.py
+++ app.py
@@ -13,26 +13,35 @@ dbname = environ.get('DB_NAME')
 user = environ.get('DB_USER')
 password = environ.get('DB_PASSWORD')
 
-print("db",environ.get('DB_HOST'))
-print("db",environ.get('DB_PORT'))
-print("db",environ.get('DB_NAME'))
-print("db",environ.get('DB_USER'))
-print("pass",password)
-
 def getConnection():
     conn = connect(dbname=dbname, user=user,password=password, host=host, port=port )
     return conn
 
-@app.get('/')
-def home():
-    conn = getConnection()
-    cursor = conn.cursor()
+@app.get('/api/users')
+def get_users():
+    return 'getting users'
 
-    cursor.execute("SELECT 1 + 1")
-    result = cursor.fetchone()
+@app.post('/api/users')
+def create_user():
+    new_user = request.get_json()
+    username = new_user['username']
+    email = new_user['email']
+    password = new_user['password']
 
-    print(result)
-    return 'home'
+    print(username, email, password)
+    return 'creating users'
+
+@app.delete('/api/users/:id')
+def delete_user():
+    return 'deleting users'
+
+@app.put('/api/users/:id')
+def update_users():
+    return 'updating users'
+
+@app.get('/api/users/:id')
+def get_user():
+    return 'getting user'
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
