6bb18cd244656cc096b8f550b1ee5c565e083557
yoelferreyra <yoelferreyra24@gmail.com>
2024-04-12 01:28:51 -0300

    Adding database schema and routes post, get

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from os import environ
 
 load_dotenv()
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 host = environ.get('DB_HOST')
 port = environ.get('DB_PORT')
@@ -13,13 +13,34 @@ dbname = environ.get('DB_NAME')
 user = environ.get('DB_USER')
 password = environ.get('DB_PASSWORD')
 
+
 def getConnection():
-    conn = connect(dbname=dbname, user=user,password=password, host=host, port=port )
+    conn = connect(dbname=dbname, user=user,
+                   password=password, host=host, port=port)
     return conn
+
 
 @app.get('/api/users')
 def get_users():
     return 'getting users'
+
+
+@app.get('/api/users/<int:id>')
+def get_user(id):
+    conn = getConnection()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM users WHERE id = %s', (id,))
+    user = cursor.fetchone()
+    print(user)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    if user is None:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify(user)
+
 
 @app.post('/api/users')
 def create_user():
@@ -28,20 +49,28 @@ def create_user():
     email = new_user['email']
     password = new_user['password']
 
-    print(username, email, password)
+    conn = getConnection()
+    cursor = conn.cursor()
+
+    cursor.execute('INSERT INTO users (username, email, password) VALUES (%s, %s, %s)',
+                   (username, email, password))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
     return 'creating users'
+
 
 @app.delete('/api/users/:id')
 def delete_user():
     return 'deleting users'
 
+
 @app.put('/api/users/:id')
 def update_users():
     return 'updating users'
 
-@app.get('/api/users/:id')
-def get_user():
-    return 'getting user'
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
